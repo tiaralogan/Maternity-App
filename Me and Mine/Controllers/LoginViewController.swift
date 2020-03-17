2fe2f2d6230eeeef89ba36096cec5f0ab08f11c5
Amaya Finklea <acfinkle@syr.edu>
2020-03-17 18:27:14 -0400

    Add a continue action button that will take the email and password that the user just created and will go to the home page of the app

--- Me and Mine/Controllers/LoginViewController.swift
+++ Me and Mine/Controllers/LoginViewController.swift
@@ -16,31 +16,37 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     
+    // Step 1: Create an action from storyboard to this class
+       // using a continue action button -- add only email and password and making sure to handle an error if needed
+       // Step 2: In action call function login()
+    @IBAction func ContinueButton(_ sender: Any) {
+        guard let email = email.text else {
+                   print("There is an issue")
+                   return
+               }
+        guard let password = password.text else {
+                   print("There is an issue")
+                   return
+               }
+        // Sign in user with an email and a password
+        Auth.auth().signIn(withEmail: email, password: password) {(user,error) in
+            if error == nil{
+                print("There is an error")
+                
+                   }
+            else {
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+    }
+    }
     
     override func viewDidLoad(){
         super.viewDidLoad()
     }
     
-    // Step 1: Create an action from storyboard to this class
-    // using a continue action button -- add only email and password and making sure to handle an error if needed 
-    // Step 2: In action call function login()
-    
-    
-    func login(){
-        guard let email = email.text else {
-            print("There is an issue")
-            return
-        }
-        guard let password = password.text else {
-            print("There is an issue")
-            return
-        }
-        // Sign in user with an email and a password
-        Auth.auth().signIn(withEmail: email, password: password) {[weak self] authResult,error in guard self != nil else {
-            return
-            }
-            
-        }
-        
-    }
+   
 }
