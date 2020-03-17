caab3e92b6771e6cf0c9261b0bb36df036ef809a
Amaya Finklea <acfinkle@syr.edu>
2020-03-17 16:29:40 -0400

    New login view controller

--- Me and Mine/Controllers/LoginViewController.swift
+++ Me and Mine/Controllers/LoginViewController.swift
@@ -13,14 +13,27 @@ import FirebaseAuth
 class LoginViewController: UIViewController {
 //Mark Properties
 //    @IBOutlet weak var MineAndMineLabel: UILabel!
-    @IBOutlet weak var usernameOrEmail: UITextField!
+    @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
    
-    
 
-override func viewDidLoad()
-{
+override func viewDidLoad(){
     super.viewDidLoad()
     }
+    
+    func login(){
+       guard let email = email.text else {
+            print("There is an issue")
+            return
+        }
+        guard let password = password.text else {
+            print("There is an issue")
+            return
+        }
+        // Sign in user with an email and a password
+        Auth.auth().signIn(withEmail: email, password: password) {[weak self] authResult,error in guard self != nil else {return}
+       
+    }
 
+}
 }
