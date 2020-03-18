1af2652b184ba8ed65e5bb3b7d9e86d0cb00d6a0
Amaya Finklea <acfinkle@syr.edu>
2020-03-18 17:19:19 -0400

    I tried to fix all the segues going in and out of the display sos page - some work, some don't. The sos label doesn't show up for some reason and back to main button doesn't go back to the home page

--- Me and Mine/Controllers/SOSDisplayViewController.swift
+++ Me and Mine/Controllers/SOSDisplayViewController.swift
@@ -11,11 +11,23 @@ import UIKit
 class SOSDisplayViewController: UIViewController {
 //MARK: Properties
     
+    @IBOutlet weak var sosLabel: UILabel!
+    @IBOutlet weak var docName: UILabel!
+    @IBOutlet weak var docEmail: UILabel!
+    @IBOutlet weak var docEmerNum: UILabel!
+    @IBOutlet weak var docAddress: UILabel!
+    @IBOutlet weak var docCity: UILabel!
+    @IBOutlet weak var docState: UILabel!
+    @IBOutlet weak var docZip: UILabel!
     
+    @IBAction func EditButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func backToMain(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
