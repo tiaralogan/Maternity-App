226bf612987cefc2dd2a312809cdb2d3c1db97c6
Amaya Finklea <acfinkle@syr.edu>
2020-03-19 16:16:54 -0400

    no message

--- Me and Mine/Controllers/PersonalViewController.swift
+++ Me and Mine/Controllers/PersonalViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import Foundation
 import EventKit
+import FirebaseAuth
+
 
 protocol UIPickerViewDataSource: class {
     func numberOfComponents(in pickerView: UIPickerView) -> Int
@@ -18,8 +20,12 @@ protocol UIPickerViewDataSource: class {
 
 class PersonalViewController: UIViewController, UIPickerViewDelegate{
     
-    //Step 1: Add the countdown to the personal page so mother is able to add the countdown to the baby's arrival
+    //Step 1: Get the username from the user profile in create account to display in the section that says USERNAME -- might need to use realtime database to store the user profile in and to fetch it
     @IBOutlet weak var avatarName: UILabel!
+    
+    //Step 2: Add the countdown to the personal page so mother is able to add the countdown to the baby's arrival
+    
+    //Step3: Add a section where the user can say whether they are having a baby girl, a baby boy, or its a surprise and they don't know
     @IBOutlet weak var babyGender: UITextField!
     var dataObject: [String] = ["Baby Girl","Baby Boy", "A Surprise"]
     
@@ -29,6 +35,7 @@ class PersonalViewController: UIViewController, UIPickerViewDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Step3: Add a section where the user can say whether they are having a baby girl, a baby boy, or its a surprise and they don't know
         let picker = UIPickerView()
         picker.delegate = self
         self.babyGender.inputView = picker
@@ -50,7 +57,16 @@ class PersonalViewController: UIViewController, UIPickerViewDelegate{
             self.babyGender.text = self.dataObject[row];
             self.babyGender.endEditing(true)
         }
+        //Step5: Add a swipe gesture so the personal page can swipe from the appointments page and the planning page
+     
     }
-    // Step3: Add a reminder section to help remind the user to do things
+    //Step5: Add a swipe gesture so the personal page can swipe from the appointments page and the planning page
+    
+    @IBAction func swipeToPlanning(_ gestureRecognizer: UISwipeGestureRecognizer) {
+    if gestureRecognizer.state == .ended { //Perform Action}
+    }
+    }
+    // Step4: Add a reminder section to help remind the user to do things
+    
     
 }// END OF CLASS
