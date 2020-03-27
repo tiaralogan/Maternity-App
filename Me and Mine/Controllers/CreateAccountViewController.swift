148f67edbc905058d339fc9a01e9079f38d4a425
Amaya Finklea <acfinkle@syr.edu>
2020-03-27 00:00:24 -0400

    Almost got the setting page up. Got to see why the table view cells are not showing up.

--- Me and Mine/Controllers/CreateAccountViewController.swift
+++ Me and Mine/Controllers/CreateAccountViewController.swift
@@ -89,19 +89,6 @@ class CreateAccountViewController: UIViewController {
     override func viewDidLoad(){
         super.viewDidLoad()}
     
-    // MARK: - Navigation
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    /*  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     if (segue.identifier == "CreateAccountSegue") {
-     
-     }
-     /*  Main.storyboard.identifier: CreateAccountSegue
-     if segue.identifier == "CreateAccountSegue" {
-     
-     // email.text password.text */
-     } */
     
     
 }
---
+++ Me and Mine/View/SettingCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingCell.swift
+//  Me and Mine
+//
+//  Created by Amaya Finklea on 3/26/20.
+//  Copyright © 2020 Logan. All rights reserved.
+//
+
+import UIKit
+
+class SettingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
